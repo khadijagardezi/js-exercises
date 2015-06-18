4c3e222a7bfd4b3991c38d528d03cf0504b7cb0e
Mashhod Rastgar <mashhoodr@gmail.com>
2015-06-18 11:06:53 +0500

    exercise10

---
+++ exercises/exercise10.js
@@ -0,0 +1,16 @@
+/**
+ * Given two input strings, write a function to find largest identical substring
+ * For example:
+ * String
+ * Spring
+ * Here the largest substring is 'ring'
+ *
+ * In case there are multiple largest substrings of similar length, the function must return the first largest substring found w.r.t first string, For example:
+ * hirespring
+ * ringfhire
+ * Here, the largest substring returned will be 'hire' and NOT 'ring'
+*/
+
+function largestSubstring(str1, str2) {
+	return '';
+}
