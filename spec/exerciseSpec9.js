f7ca3bafe1532a7508d0a996da7d8971c0567e04
abdulqadir93 <abdulqadir@marketlytics.com>
2015-06-22 23:27:03 +0500

    add spec for exercise 9

--- spec/exerciseSpec9.js
+++ spec/exerciseSpec9.js
@@ -1,24 +1,75 @@
-/*
-expect(telephoneCheck("555-555-5555")).to.be.a("boolean");
-assert.deepEqual(telephoneCheck("1 555-555-5555"), true);
-assert.deepEqual(telephoneCheck("1 (555) 555-5555"), true);
-assert.deepEqual(telephoneCheck("5555555555"), true);
-assert.deepEqual(telephoneCheck("555-555-5555"), true);
-assert.deepEqual(telephoneCheck("(555)555-5555"), true);
-assert.deepEqual(telephoneCheck("1(555)555-5555"), true);
-assert.deepEqual(telephoneCheck("1 555 555 5555"), true);
-assert.deepEqual(telephoneCheck("555-555-5555"), true);
-assert.deepEqual(telephoneCheck("1 456 789 4444"), true);
-assert.deepEqual(telephoneCheck("123**&!!asdf#"), false);expected true to deeply equal false
-assert.deepEqual(telephoneCheck("55555555"), false);expected true to deeply equal false
-assert.deepEqual(telephoneCheck("(6505552368)"), false);expected true to deeply equal false
-assert.deepEqual(telephoneCheck("2 (757) 622-7382"), false);expected true to deeply equal false
-assert.deepEqual(telephoneCheck("0 (757) 622-7382"), false);expected true to deeply equal false
-assert.deepEqual(telephoneCheck("-1 (757) 622-7382"), false);expected true to deeply equal false
-assert.deepEqual(telephoneCheck("2 757 622-7382"), false);expected true to deeply equal false
-assert.deepEqual(telephoneCheck("10 (757) 622-7382"), false);expected true to deeply equal false
-assert.deepEqual(telephoneCheck("27576227382"), false);expected true to deeply equal false
-assert.deepEqual(telephoneCheck("(275)76227382"), false);expected true to deeply equal false
-assert.deepEqual(telephoneCheck("2(757)6227382"), false);expected true to deeply equal false
-assert.deepEqual(telephoneCheck("2(757)622-7382"), false);expected true to deeply equal false
-*/
+describe("telephoneCheck", function() {
+	
+	it("valid number with '-' seperator", function() {
+		expect(telephoneCheck("555-555-5555")).toBe(true);
+	});
+	
+	it("valid number with '-' seperated and country code", function() {
+		expect(telephoneCheck("1 555-555-5555")).toBe(true);
+	});
+	
+	it("valid number with '-' seperator, enclosed first 3 digit chunk and country code", function() {
+		expect(telephoneCheck("1 (555) 555-5555")).toBe(true);
+	});
+	
+	it("valid number with no seperators and no country code", function() {
+		expect(telephoneCheck("5555555555")).toBe(true);
+	});
+	
+	it("valid number with enclosed first 3 digit chunk and '-' seperator", function() {
+		expect(telephoneCheck("(555)555-5555")).toBe(true);
+	});
+	
+	it("valid number with enclosed first 3 digit chunk, '-' seperator and country code", function() {
+		expect(telephoneCheck("1(555)555-5555")).toBe(true);
+	});
+	
+	it("valid number with country code and random digits with ' ' seperator", function() {
+		expect(telephoneCheck("1 456 789 4444")).toBe(true);
+	});
+	
+	it("invalid number with alpha numeric characters", function() {
+		expect(telephoneCheck("123**&!!asdf#")).toBe(false);
+	});
+	
+	it("invalid number having less than 10 digits", function() {
+		expect(telephoneCheck("55555555")).toBe(false);
+	});
+	
+	it("invalid number with no seperators and having wrong country code", function() {
+		expect(telephoneCheck("27576227382")).toBe(false);
+	});
+	
+	it("invalid number having wrong number of eclosed digits", function() {
+		expect(telephoneCheck("(6505552368)")).toBe(false);
+	});
+	
+	it("invalid number with country code greater than 1, eclosed first 3 digit chunk and '-' seperator", function() {
+		expect(telephoneCheck("2 (757) 622-7382")).toBe(false);
+	});
+	
+	it("invalid number with country code less than 1, eclosed first 3 digit chunk and '-' seperator", function() {
+		expect(telephoneCheck("0 (757) 622-7382")).toBe(false);
+	});
+	
+	it("invalid number with country code less than 1, eclosed first 3 digit chunk and '-' seperator", function() {
+		expect(telephoneCheck("-1 (757) 622-7382")).toBe(false);
+	});
+	
+	it("invalid number with wrongly encapsulating the first 3 digit chunk which includes the country code", function() {
+		expect(telephoneCheck("(175)76227382")).toBe(false);
+	});
+	
+	it("invalid number with wrong country code and enclosed first 3 digit chunk", function() {
+		expect(telephoneCheck("2(757)6227382")).toBe(false);
+	});
+	
+	it("invalid number with wrong country code, enclosed first 3 digit chunk and '-' seperator", function() {
+		expect(telephoneCheck("2(757)622-7382")).toBe(false);
+	});
+	
+	it("invalid number with space between last 4 digit chunk", function() {
+		expect(telephoneCheck("1(757) 622-73 82")).toBe(false);
+	});
+	
+});
