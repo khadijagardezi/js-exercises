8105b4067192987a0f5a09d77d74ccc59deb7973
Mashhood Rastgar <mashhoodr@gmail.com>
2015-06-23 10:59:11 +0500

    more fixes

--- spec/exercise5Spec.js
+++ spec/exercise5Spec.js
@@ -9,11 +9,11 @@ describe("isPalidrome", function() {
 	});
 
 	it("with multi word palindrome", function() {
-		expect(isPalindrome("was it a car or a cat i saw")).toBe(true);
+		expect(isPalindrome("was it a car or a cat i saw")).toBe(false);
 	});
 
 	it("with multi word palindrome and mix caps", function() {
-		expect(isPalindrome("Was it a car or a cat I saw")).toBe(true);
+		expect(isPalindrome("Was it a car or a cat I saw")).toBe(false);
 	});
 
 	it("with invalid palindrome", function() {
