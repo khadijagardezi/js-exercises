55c45386837fd4415ee64014027934f2277b4c5f
Mashhood Rastgar <mashhoodr@gmail.com>
2015-06-23 09:49:39 +0500

    fixed spec for exercise 2

--- spec/exercise2Spec.js
+++ spec/exercise2Spec.js
@@ -1,17 +1,17 @@
 describe("filterLongWords", function() {
 
   it("with an empty array", function() {
-    var result = findLongestWord([], 1);
+    var result = filterLongWords([], 1);
     expect(typeof result).toBe('array');
     expect(result).toBe([]);
   });
 
   it("with the 2 words bigger than 2", function() {
-    expect(findLongestWord(['a', 'as', 'fds', 'asas', 'a', 'as'], 2)).toBe(['fds','asas']);
+    expect(filterLongWords(['a', 'as', 'fds', 'asas', 'a', 'as'], 2)).toBe(['fds','asas']);
   });
 
   it("with no large words", function() {
-    expect(findLongestWord(['as', 'asas', 'dswd', 'as', 'a'], 5)).toBe([]);
+    expect(filterLongWords(['as', 'asas', 'dswd', 'as', 'a'], 5)).toBe([]);
   });
 
 });
